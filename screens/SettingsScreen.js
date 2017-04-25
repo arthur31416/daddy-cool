9f5ace368d074b45cac7c7924b1e1efa7677792b
Arthur <arthur.jsx@gmail.com>
2017-04-25 14:41:49 +0200

    Added (mute) player

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,41 +1,46 @@
 // @flow
 
-import React, { Component } from 'react';
-import Router from '../navigation/Router'
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableHighlight
-} from 'react-native';
-import { withNavigation } from '@expo/ex-navigation';
-import Colors from '../constants/Colors';
-import { metrics } from '../themes';
+import React, { Component } from "react";
+import Router from "../navigation/Router";
+import { ReactNativeAudioStreaming, Player } from "react-native-audio-streaming";
+import { View, Text, StyleSheet, TouchableHighlight } from "react-native";
+import { withNavigation } from "@expo/ex-navigation";
+import Colors from "../constants/Colors";
+import { metrics } from "../themes";
 
 type Props = {
   navigator: Object
 };
 
+const url = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-10.mp3";
+
 @withNavigation
 export default class SettingsScreen extends Component {
-  props: Props
+  props: Props;
 
   static route = {
     navigationBar: {
-      title: 'Profile'
+      title: "Profile"
     }
   };
 
-  _goToSearch = () => {
-    this.props.navigation.performAction(({ tabs }) => {
-      tabs('main').jumpToTab('search');
+  componentDidMount () {
+    ReactNativeAudioStreaming.play(url, {
+      showIniOSMediaCenter: true,
+      showInAndroidNotifications: true
     });
   }
+
+  _goToSearch = () => {
+    this.props.navigation.performAction(({ tabs }) => {
+      tabs("main").jumpToTab("search");
+    });
+  };
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.blankText}>
+        {/*<Text style={styles.blankText}>
           You don't have bookmarked books yet
         </Text>
 
@@ -43,7 +48,9 @@ export default class SettingsScreen extends Component {
           <Text style={styles.link}>
             Find books
           </Text>
-        </TouchableHighlight>
+        </TouchableHighlight>*/}
+
+        <Player url={url} />
       </View>
     );
   }
@@ -52,16 +59,16 @@ export default class SettingsScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center"
   },
   blankText: {
     fontSize: 16,
-    color: '#666'
+    color: "#666"
   },
   link: {
     fontSize: 18,
     color: Colors.primary,
     marginTop: metrics.smallPadding
   }
-})
+});
